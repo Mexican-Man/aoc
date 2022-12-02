23c183f6cb6a45ab13cf52d634807ebeb1ff1fca
Cole Crouter <colecrouter@hotmail.com>
2022-12-01 18:41:26 -0600

    Day 1

---
+++ README.md
@@ -0,0 +1,13 @@
+# Advent of Code 2022
+
+![](https://img.shields.io/badge/day%20📅-22-blue)
+![](https://img.shields.io/badge/stars%20⭐-34-yellow)
+![](https://img.shields.io/badge/days%20completed-17-red)
+
+
+This repository contains my solutions to the [Advent of Code 2022](https://adventofcode.com/2022) challenges.
+
+## Usage
+```
+npx ts-node-esm src/<day>.ts
+```
---
+++ src/1.ts
@@ -0,0 +1,37 @@
+const input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+type Food = number;
+type Elf = Array<Food>;
+
+const splitInput = input
+    .split('\n\n')
+    .map((group) => group
+        .split('\n')
+        .map((line) => parseInt(line, 10) as Food) as Elf);
+
+const elvesTotalCalories = splitInput
+    .map((elf) => elf
+        .reduce((calorieCount, food) => calorieCount + food, 0));
+
+const elvesTotalSorted = elvesTotalCalories.sort((a, b) => b - a);
+
+const top1 = elvesTotalSorted[0];
+
+const top3Acc = elvesTotalSorted
+    .slice(0, 3)
+    .reduce((total, calories) => total + calories, 0);
+
+console.log(`Part 1: ${top1}\nPart 2: ${top3Acc}`);
